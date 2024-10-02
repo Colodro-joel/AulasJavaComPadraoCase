d115dcf8d043e84f6964f7888acb9585a7fe1210
Aluno <joelcolodro@gmail.com>
2024-10-02 09:30:32 -0300

    aula java 02/10

---
+++ src/Aula3/Exemplos/Exemplos.java
@@ -0,0 +1,22 @@
+package Aula3.Exemplos;
+
+public class Exemplos {
+    public static void main(String[] args) {
+       int anoNasc;
+       double peso;
+       char sexo;
+       boolean canhoto;
+       float altura;
+       byte idade;
+
+       anoNasc = 2004;
+       peso = 65.00;
+       sexo = 'M';
+       altura = 1.8f;
+       idade = 20;
+
+       anoNasc++;
+
+        System.out.println(anoNasc);
+    }
+}
--- src/Exercicios/Exemplos.java
+++
@@ -1,7 +0,0 @@
-package Exercicios;
-
-public class Exemplos {
-    public static void main(String[] args) {
-        System.out.println("Olá");
-    }
-}
