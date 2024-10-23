03fce3602755f90a3aca20b2def726226862ba49
Aluno <joelcolodro@gmail.com>
2024-10-23 08:28:45 -0300

    Exemplo1 aula4

---
+++ src/Aula4/Exemplo1.java
@@ -0,0 +1,17 @@
+package Aula4;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+
+            int idade = 18;
+
+            if (idade >= 18) {
+                System.out.println("Acesso permitido");
+
+            } else {
+                System.out.println("Acesso negado");
+            }
+
+    }
+
+}
